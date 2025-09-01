07ae517c506203d1c17316352752e0e6b7e73782
n <nataliaklucnikova9@gmail.com>
2025-09-01 14:05:35 +0300

    .

---
+++ example_modal_carusel/src/pages/Main.tsx
@@ -0,0 +1,71 @@
+import React from 'react';
+
+export const Main = () =>{
+
+    return(
+        <div className="example_page">
+            <button type="button" className="btn btn-primary">Primary</button>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
+        </div>
+    )
+}
