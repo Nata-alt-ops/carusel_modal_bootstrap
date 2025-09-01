158c03559d109c49fa91cde3083bf0599f852c59
n <nataliaklucnikova9@gmail.com>
2025-09-01 14:25:49 +0300

    .

--- example_modal_carusel/src/App.tsx
+++ example_modal_carusel/src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { Main } from './pages';
+import { Main } from './pages/main';
 
 function App() {
   return (
