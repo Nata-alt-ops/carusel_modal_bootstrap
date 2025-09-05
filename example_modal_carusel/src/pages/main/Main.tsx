388340b0ad1382627b39458e238046d928de37f5
n <nataliaklucnikova9@gmail.com>
2025-09-05 09:41:12 +0300

    .

--- example_modal_carusel/src/pages/main/Main.tsx
+++ example_modal_carusel/src/pages/main/Main.tsx
@@ -207,11 +207,11 @@ useEffect(() => {
 <div className="accordion" id="accordionExample">
   <div className="accordion-item">
     <h2 className="accordion-header" id="headingOne">
-      <button className="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+      <button className="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="false" aria-controls="collapseOne">
         Элемент аккордеона #1
       </button>
     </h2>
-    <div id="collapseOne" className="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+    <div id="collapseOne" className="accordion-collapse collapse" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
       <div className="accordion-body">
         Если ты не голубой подрисуй вагон другой
       </div>
